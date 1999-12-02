38455a662704a86ed8c89582f18cd5c533bc2fd2
NeXus Developers <nexus-developers@nexusformat.org>
1999-12-02 02:27:16 +0000

    - Moved NXfcompress and NXcompress functions togetherRay
    git-svn-id: https://svn.nexusformat.org/code/trunk@91 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- src/napi.c
+++ src/napi.c
@@ -679,11 +679,6 @@ static const char* rscid = "$Id$";	/* Revision interted by CVS */
     return NX_OK;
   }
   
-  NXstatus NXfcompress(NXhandle fid, int *compr_type)
-  { 
-      return NXcompress(fid,*compr_type);
-  }
-  
   NXstatus NXfmakedata(NXhandle fid, char *name, int *pDatatype,
 		int *pRank, int dimensions[])
   {
@@ -836,6 +831,55 @@ static const char* rscid = "$Id$";	/* Revision interted by CVS */
     return NX_OK;
   }
   
+  
+  NXstatus NXfcompress(NXhandle fid, int *compr_type)
+  { 
+      return NXcompress(fid,*compr_type);
+  }
+  
+  NXstatus
+  NXcompress (NXhandle fid, int compress_type)
+  {
+    pNexusFile pFile;
+    int32 iRank, iAtt, iType, iRet, i, e;
+    int32 iSize[MAX_VAR_DIMS];
+    NXname pBuffer;
+    char pError[512];
+    comp_info compstruct;  
+    char *str;
+
+    pFile = NXIassert (fid);
+  
+    /* check if there is an SDS open */
+    if (pFile->iCurrentSDS == 0) {
+      NXIReportError (NXpData, "ERROR: no SDS open");
+      return NX_ERROR;
+    }
+
+    /* first read dimension information */
+    SDgetinfo (pFile->iCurrentSDS, pBuffer, &iRank, iSize, &iType, &iAtt);
+
+    /* 
+       according to compression type initialize compression
+       information 
+    */
+    if(compress_type == NX_COMP_LZW)
+    {
+         compstruct.deflate.level = 6;
+    }
+    else if(compress_type == NX_COMP_HUF)
+    {
+        compstruct.skphuff.skp_size = DFKNTsize(iType);
+    }
+
+    iRet = SDsetcompress(pFile->iCurrentSDS, compress_type, &compstruct);
+    if (iRet < 0) {
+      sprintf (pError, "ERROR: failure to compress data to %s", pBuffer);
+      NXIReportError (NXpData, pError);
+      return NX_ERROR;
+    }
+    return NX_OK;
+  }  
   
   NXstatus
   NXclosedata (NXhandle fid)
@@ -1006,51 +1050,6 @@ static const char* rscid = "$Id$";	/* Revision interted by CVS */
     }
     return NX_OK;
   }
-  
-  NXstatus
-  NXcompress (NXhandle fid, int compress_type)
-  {
-    pNexusFile pFile;
-    int32 iRank, iAtt, iType, iRet, i, e;
-    int32 iSize[MAX_VAR_DIMS];
-    NXname pBuffer;
-    char pError[512];
-    comp_info compstruct;  
-    char *str;
-
-    pFile = NXIassert (fid);
-  
-    /* check if there is an SDS open */
-    if (pFile->iCurrentSDS == 0) {
-      NXIReportError (NXpData, "ERROR: no SDS open");
-      return NX_ERROR;
-    }
-
-    /* first read dimension information */
-    SDgetinfo (pFile->iCurrentSDS, pBuffer, &iRank, iSize, &iType, &iAtt);
-
-    /* 
-       according to compression type initialize compression
-       information 
-    */
-    if(compress_type == NX_COMP_LZW)
-    {
-         compstruct.deflate.level = 6;
-    }
-    else if(compress_type == NX_COMP_HUF)
-    {
-        compstruct.skphuff.skp_size = DFKNTsize(iType);
-    }
-
-    iRet = SDsetcompress(pFile->iCurrentSDS, compress_type, &compstruct);
-    if (iRet < 0) {
-      sprintf (pError, "ERROR: failure to compress data to %s", pBuffer);
-      NXIReportError (NXpData, pError);
-      return NX_ERROR;
-    }
-    return NX_OK;
-  }
-  
   
   NXstatus
   NXputslab (NXhandle fid, void *data, int iStart[], int iSize[])
