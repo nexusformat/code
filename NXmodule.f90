050408e97f9e28dece8db1536c6e7b65fa5ecb0f
NeXus Developers <nexus-developers@nexusformat.org>
2000-12-07 02:28:50 +0000

    - Update NeXus version number to 1.3.3 Ray
    git-svn-id: https://svn.nexusformat.org/code/trunk@143 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- NXmodule.f90
+++ NXmodule.f90
@@ -36,7 +36,7 @@ MODULE NXmodule
 
    PUBLIC
 ! *** NeXus version parameter
-   CHARACTER(len=*), PARAMETER, PUBLIC :: NeXus_version = "1.3.2"
+   CHARACTER(len=*), PARAMETER, PUBLIC :: NeXus_version = "1.3.3"
 ! *** NeXus file access parameters
    INTEGER, PARAMETER, PUBLIC :: NXACC_READ = 1
    INTEGER, PARAMETER, PUBLIC :: NXACC_RDWR = 3
