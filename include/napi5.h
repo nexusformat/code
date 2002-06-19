b6d815c19e3b4494cbf96fd299e0592cf1e6f8a6
NeXus Developers <nexus-developers@nexusformat.org>
2002-06-19 21:20:37 +0000

    - Added definition of NX5sameID.
    Ray
    
    
    git-svn-id: https://svn.nexusformat.org/code/trunk@159 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- include/napi5.h
+++ include/napi5.h
@@ -1,3 +1,27 @@
+/*---------------------------------------------------------------------------
+  NeXus - Neutron & X-ray Common Data Format
+  
+  Application Program Interface (HDF5) Header File
+  
+  Copyright (C) 1997-2002 Mark Koennecke, Przemek Klosowski
+  
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
+ 
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+ 
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+             
+  For further information, see <http://www.neutron.anl.gov/NeXus/>
+
+----------------------------------------------------------------------------*/
 #define NX5SIGNATURE 959695
 
 #include <hdf5.h>
@@ -36,6 +60,7 @@ NX_EXTERNAL  NXstatus CALLING_STYLE NX5getattr(NXhandle handle, char* name, void
 NX_EXTERNAL  NXstatus CALLING_STYLE NX5getattrinfo(NXhandle handle, int* no_items);
 NX_EXTERNAL  NXstatus CALLING_STYLE NX5getgroupID(NXhandle handle, NXlink* pLink);
 NX_EXTERNAL  NXstatus CALLING_STYLE NX5getgroupinfo(NXhandle handle, int* no_items, NXname name, NXname nxclass);
+NX_EXTERNAL  NXstatus CALLING_STYLE NX5sameID(NXhandle handle, NXlink* pFirstID, NXlink* pSecondID);
 
 NX_EXTERNAL  NXstatus CALLING_STYLE NX5initgroupdir(NXhandle handle);
 NX_EXTERNAL  NXstatus CALLING_STYLE NX5initattrdir(NXhandle handle);
