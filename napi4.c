8b87eee2e43e3fea9cc2bb88b2587d85bc66782e
NeXus Developers <nexus-developers@nexusformat.org>
2003-03-30 22:41:07 +0000

    Added CVS Id tag
    git-svn-id: https://svn.nexusformat.org/code/trunk@214 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- napi4.c
+++ napi4.c
@@ -20,6 +20,8 @@
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
              
   For further information, see <http://www.neutron.anl.gov/NeXus/>
+  
+  $Id$
 
 ----------------------------------------------------------------------------*/
 #include <stdlib.h>
