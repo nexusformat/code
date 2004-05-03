08ba8c6fc46bbb6c5d3e5b0f16799987a3e2fd95
Jens Krueger <jens.krueger@frm2.tum.de>
2004-05-03 11:29:56 +0000

    Add the search of autoconf-2.52
    git-svn-id: https://svn.nexusformat.org/code/trunk@280 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- autogen.sh
+++ autogen.sh
@@ -23,7 +23,7 @@ case $automake in
 		ACLOCAL="aclocal";
 esac
 	
-for autoconf in autoconf-2.59 autoconf-2.57 autoconf-2.53 autoconf ; do 
+for autoconf in autoconf-2.59 autoconf-2.57 autoconf-2.53 autoconf-2.52 autoconf ; do 
 	AUTOCONF=`which $autoconf 2>/dev/null`
 	if test "$AUTOCONF" ; then
 		break;
