a5b25766c5fcda1a7eb43dc6bf27c8e7488647ab
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2004-05-28 09:55:53 +0000

    Move check for configure version to a separate file so that it can also be run from configure to ensure version consistancy
    git-svn-id: https://svn.nexusformat.org/code/trunk@313 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- autogen.sh
+++ autogen.sh
@@ -8,35 +8,10 @@ for libtoolize in glibtoolize libtoolize ; do
 done
 LIBTOOLIZE="$libtoolize --force --copy --automake"
 
-for automake in automake-1.8 automake-1.7 automake-1.6 automake-1.5 automake-1.4 automake ; do
-	AUTOMAKE=`which $automake 2>/dev/null |  grep -v '^no'`
-	if test "$AUTOMAKE" ; then
-		break;
-	fi
-done
-AUTOMAKE="$automake -a -c"
-case $automake in
-	automake-*)
-		version=`echo $automake | cut -f2 -d'-'`
-		ACLOCAL="aclocal-$version";;
-	*)
-		ACLOCAL="aclocal";
-esac
-	
-for autoconf in autoconf-2.59 autoconf-2.57 autoconf-2.53 autoconf-2.52 autoconf ; do 
-	AUTOCONF=`which $autoconf 2>/dev/null | grep -v '^no'`
-	if test "$AUTOCONF" ; then
-		break;
-	fi
-done
-AUTOCONF="$autoconf"
-case $autoconf in
-	autoconf-*)
-		version=`echo $autoconf | cut -f2 -d'-'`
-		AUTOHEADER="autoheader-$version";;
-	*)
-		AUTOHEADER="autoheader";;
-esac
+# set up environment variables with the correct
+# version of autoconf and automake - this also needs
+# to be done in configure.ac to maintain consistancy
+. autoversion.sh
 
 touch AUTHORS ChangeLog NEWS README
 
---
+++ autoversion.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+for automake in automake-1.8 automake-1.7 automake-1.6 automake-1.5 automake-1.4 automake ; do
+	AUTOMAKE=`which $automake 2>/dev/null |  grep -v '^no'`
+	if test "$AUTOMAKE" ; then
+		break;
+	fi
+done
+AUTOMAKE="$automake -a -c"
+case $automake in
+	automake-*)
+		version=`echo $automake | cut -f2 -d'-'`
+		ACLOCAL="aclocal-$version";;
+	*)
+		ACLOCAL="aclocal";
+esac
+	
+for autoconf in autoconf-2.59 autoconf-2.57 autoconf-2.53 autoconf-2.52 autoconf ; do 
+	AUTOCONF=`which $autoconf 2>/dev/null | grep -v '^no'`
+	if test "$AUTOCONF" ; then
+		break;
+	fi
+done
+AUTOCONF="$autoconf"
+case $autoconf in
+	autoconf-*)
+		version=`echo $autoconf | cut -f2 -d'-'`
+		AUTOHEADER="autoheader-$version";;
+	*)
+		AUTOHEADER="autoheader";;
+esac
+
+export AUTOMAKE ACLOCAL AUTOCONF AUTOHEADER
+
