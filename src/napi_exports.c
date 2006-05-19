bba5c526991f9e03f4181d4f8db77966adb1c10b
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2006-05-19 08:24:14 +0000

    Extras for MinGW
    git-svn-id: https://svn.nexusformat.org/code/trunk@776 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

---
+++ src/napi_exports.c
@@ -0,0 +1,10 @@
+/*
+ * $Id$
+ *
+ *  __stdcall uppercase aliases for Windows
+ */
+
+#include "napi.h"
+
+#define CALL_MODE	__stdcall
+#include "napi_exports.h"
---
+++ src/napi_exports2.c
@@ -0,0 +1,10 @@
+/*
+ * $Id$
+ *
+ *  __cdecl uppercase aliases for Windows
+ */
+
+#include "napi.h"
+
+#define CALL_MODE	__cdecl
+#include "napi_exports.h"
