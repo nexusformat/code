0594fd3ca651d376d6c8d067acbb32b048e89dba
Mark Koennecke <Mark.Koennecke@psi.ch>
2006-12-18 08:13:02 +0000

    - Commited package renamed Java API, refs #2
    git-svn-id: https://svn.nexusformat.org/code/trunk@844 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

---
+++ bindings/java/org/nexusformat/NXlink.java
@@ -0,0 +1,26 @@
+/**
+  * NXlink is a replacement for the structure holding the data necessary
+  * for doing a link in the NeXus-API. Consequently it is primitive.
+  *
+  * Mark Koennecke, October 2000
+  * 
+  * updated for Napi-2.0 with HDF-5
+  * Mark Koennecke, August 2001
+  *
+  * updated for NAPI-3.0 with XML
+  * Mark Koennecke, October 2004
+  *
+  * copyright: see accompanying COPYRIGHT file 
+  */
+package org.nexusformat;
+
+public class NXlink {
+    public int tag, ref;
+    public String ref5, refd, targetPath;
+   
+    public NXlink(){
+       ref5 = new String("");
+       refd = new String("");
+       targetPath = new String("");
+    }
+}
