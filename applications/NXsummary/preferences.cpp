a3fd2f783f5ec8e39e445c86bbdf5bf2b7c46203
Peter Peterson <petersonpf@ornl.gov>
2007-03-14 19:46:12 +0000

    First version that can read configuration from a file. Refs #30.
    git-svn-id: https://svn.nexusformat.org/code/trunk@866 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- applications/NXsummary/preferences.cpp
+++ applications/NXsummary/preferences.cpp
@@ -1,11 +1,24 @@
+#include <iostream>
 #include <libxml/parser.h>
 #include <libxml/tree.h>
+#include <sstream>
+#include <stdexcept>
 #include "preferences.hpp"
 
+using std::cout;
+using std::endl;
+using std::runtime_error;
 using std::string;
+using std::ostringstream;
 using std::vector;
 
 static const int BUFFER_SIZE = 256;
+static const xmlChar *root_name = xmlCharStrdup("nxsummary");
+static const xmlChar *item_name = xmlCharStrdup("item");
+static const xmlChar *path_name = xmlCharStrdup("path");
+static const xmlChar *label_name = xmlCharStrdup("label");
+static const xmlChar *operation_name = xmlCharStrdup("operation");
+
 namespace nxsum {
   void addItem(vector<Item> &preferences, const string &path,
                const string &label) {
@@ -45,6 +58,84 @@ static const string PI_LABEL = "PRICINPLE INVESTIGATOR";
   */
   }
 
+  void loadPreferences(std::string &filename, xmlDocPtr &doc) {
+    xmlLineNumbersDefault(1);
+    doc = xmlReadFile(filename.c_str(), NULL, 0);
+    if (doc == NULL)
+      {
+        ostringstream s;
+        s << "Could not read configuration file \"" << filename << "\"";
+        throw runtime_error(s.str());
+      }
+  }
+
+  void cleanupXml(xmlDocPtr &doc) {
+    xmlFreeDoc(doc);
+    xmlCleanupParser();
+  }
+
+  void loadPreference(xmlNodePtr &item_node, vector<Item> &preferences) {
+    if (item_node == NULL)
+      {
+        return;
+      }
+    if (item_node->type != XML_ELEMENT_NODE)
+      {
+        return;
+      }
+    if (! xmlStrEqual(item_node->name, item_name))
+      {
+        return;
+      }
+    if (item_node->properties == NULL)
+      {
+        return;
+      }
+    Item item;
+    if (xmlHasProp(item_node, path_name))
+      {
+        item.path = (char *) xmlGetProp(item_node, path_name);
+      }
+    else
+      {
+        ostringstream s;
+        s << "Could not find path attribute in configuration file (line ";
+        s << xmlGetLineNo(item_node) << ")";
+        throw runtime_error(s.str());
+      }
+    if (xmlHasProp(item_node, label_name))
+      {
+        item.label = (char *) xmlGetProp(item_node, label_name);
+      }
+    else
+      {
+        ostringstream s;
+        s << "Could not find label attribute in configuration file (line ";
+        s << xmlGetLineNo(item_node) << ")";
+        throw runtime_error(s.str());
+      }
+    if (xmlHasProp(item_node, operation_name))
+      {
+        item.operation = (char *)xmlGetProp(item_node, operation_name);
+      }
+
+    preferences.push_back(item);
+  }
+
+  void loadPreferences(xmlNodePtr &root_node, vector<Item> &preferences) {
+    xmlNodePtr cur_node = NULL;
+
+    for (cur_node = root_node; cur_node; cur_node = cur_node->next) {
+      if ((cur_node->type == XML_ELEMENT_NODE)
+                                && xmlStrEqual(cur_node->name, root_name)) {
+        for (cur_node = cur_node->children; cur_node; cur_node = cur_node->next) {
+          loadPreference(cur_node, preferences);
+        }
+        return;
+      }
+    }
+  }
+
   void loadPreferences(std::string &filename, std::vector<Item> &preferences) {
     if (filename.size() <= 0)
       {
@@ -53,12 +144,10 @@ static const string PI_LABEL = "PRICINPLE INVESTIGATOR";
     else
       {
         xmlDocPtr doc = NULL; // document pointer
-        xmlNodePtr root_node = NULL;
-        xmlNodePtr node = NULL;
-        xmlNodePtr node1 = NULL;
-        char bufffer[BUFFER_SIZE];
-        int i,j;
-        // do nothing for now with the provided file
+        loadPreferences(filename, doc);
+        xmlNodePtr root_node = xmlDocGetRootElement(doc);
+        loadPreferences(root_node, preferences);
+        cleanupXml(doc);
       }
   }
 }
