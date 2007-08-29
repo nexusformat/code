0a40dd3472e70b07bfe9bf416ade16f6abcd0b12
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2007-08-29 09:23:14 +0000

    Update version to 4.1.0 Refs #88.
    git-svn-id: https://svn.nexusformat.org/code/trunk@955 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- include/napi.h
+++ include/napi.h
@@ -29,7 +29,7 @@
 #define NEXUSAPI
 
 /* NeXus HDF45 */
-#define NEXUS_VERSION   "4.0.0"                /* major.minor.patch */
+#define NEXUS_VERSION   "4.1.0"                /* major.minor.patch */
 
 #define CONSTCHAR       const char
 
