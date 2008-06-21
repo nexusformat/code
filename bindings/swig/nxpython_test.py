937993c7b18dfc4201c3613c25a93edf962898da
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2008-06-21 10:50:37 +0000

    Add new python bindings to configure. Refs #101.
    git-svn-id: https://svn.nexusformat.org/code/trunk@1000 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- bindings/swig/nxpython_test.py
+++
@@ -1,9 +0,0 @@
-# run with
-#
-#    env PYTHONPATH=.libs python nxpython_test.py
-#
-# as  nxpython.py  needs to load  libnxpython.so
-#
-import nxpython
-h = nxpython.nx_open("nxpython_test.nxs", nxpython.NXACC_CREATE)
-nxpython.nx_close(h)
