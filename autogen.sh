5783c596132403d5bdcfc11522bdf446e9342059
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2008-07-01 16:32:10 +0000

    Add in fixes from 4.1 branch for r985:r1003 * require autoconf 2.61 and above * fix for JAVAC being defined Refs #94
    git-svn-id: https://svn.nexusformat.org/code/trunk@1004 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- autogen.sh
+++ autogen.sh
@@ -26,15 +26,13 @@ echo "Using $autoconfversion"
 echo "Using $automakeversion"
 echo "Using $libtoolversion"
 case $autoconfversion in
-    *2.5[2-79])
-	;;
     *2.6*)
 	;;
     *2.7*)
 	;;
     *)
 	echo "This autoconf version is not supported by NeXus."
-	echo "NeXus only supports autoconf 2.5[2-79]."
+	echo "NeXus only supports autoconf 2.61 and above"
 	echo "You may download it from ftp://ftp.gnu.org/gnu/autoconf"
 	exit
 	;;
@@ -47,18 +45,18 @@ case $automakeversion in
 	;;
     *)
 	echo "This automake version is not supported by NeXus."
-	echo "NeXus only supports automake 1.[5-8].*."
+	echo "NeXus only supports automake 1.4 and above"
 	echo "You may download it from ftp://ftp.gnu.org/gnu/automake"
 	exit
 	;;
 esac
 
 case $libtoolversion in
-    *1.[45]*)
+    *1.[456]*)
 	;;
     *)
 	echo "This libtool version is not supported by NeXus."
-	echo "NeXus only supports libtool 1.[45].*."
+	echo "NeXus only supports libtool 1.4 and above"
 	echo "You may download it from ftp://ftp.gnu.org/gnu/libtool"
 	exit
 	;;
