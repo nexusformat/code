cd36042e1c34718134c039361b11e7b1b7a47b44
Peter Peterson <petersonpf@ornl.gov>
2008-07-09 18:20:28 +0000

    Added enumerations for data types and compression types. Refs #116.
    git-svn-id: https://svn.nexusformat.org/code/trunk@1013 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- bindings/cpp/NeXusFile.hpp
+++ bindings/cpp/NeXusFile.hpp
@@ -5,7 +5,28 @@
 #include "napi.h"
 
 namespace NeXus {
-  //  typedef enum NXnumtype {BLAH};
+  typedef enum NXnumtype {
+    FLOAT32 = NX_FLOAT32,
+    FLOAT64 = NX_FLOAT64,
+    INT8 = NX_INT8,
+    UINT8 = NX_UINT8,
+    // BOOLEAN = NX_BOOLEAN, // NX_BOOLEAN is currently broken
+    INT16 = NX_INT16,
+    UINT16 = NX_UINT16,
+    INT32 = NX_INT32,
+    UINT32 = NX_UINT32,
+    INT64 = NX_INT64,
+    UINT64 = NX_UINT64,
+    CHAR = NX_CHAR,
+    BINARY = NX_BINARY
+  };
+
+  typedef enum NXcompression {
+    NONE = NX_COMP_NONE,
+    LZW = NX_COMP_LZW,
+    RLE = NX_COMP_RLE,
+    HUF = NX_COMP_HUF
+  };
 
   class File
   {
@@ -16,6 +37,8 @@ namespace NeXus {
     File(const std::string filename, const NXaccess access = NXACC_READ);
     
     ~File();
+
+    
   };
 };
 
