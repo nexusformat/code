98dcea7744d59b91c47f8e3384fcd6d16a69c70d
Peter Peterson <petersonpf@ornl.gov>
2008-07-09 17:45:59 +0000

    Added status member to the class. Refs #116.
    git-svn-id: https://svn.nexusformat.org/code/trunk@1011 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- bindings/cpp/NeXusException.cpp
+++ bindings/cpp/NeXusException.cpp
@@ -2,13 +2,18 @@
 
 using namespace NeXus;
 
-Exception::Exception(const std::string& msg) :
+Exception::Exception(const std::string& msg, const int status) :
   std::runtime_error(msg) {
  this->__what = msg;
+ this->__status = status;
 }
 
 const char* Exception::what() const throw() {
-  return __what.c_str();
+  return this->__what.c_str();
+}
+
+int Exception::status() throw() {
+  return this->__status;
 }
 
 Exception::~Exception() throw() {
