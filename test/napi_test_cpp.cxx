0801f7576688adb3452c0c556b272ce60144f822
Peter Peterson <petersonpf@ornl.gov>
2008-07-10 14:06:21 +0000

    Added a file open to the cpp test. Refs #116.
    git-svn-id: https://svn.nexusformat.org/code/trunk@1019 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- test/napi_test_cpp.cxx
+++ test/napi_test_cpp.cxx
@@ -1,9 +1,11 @@
 #include <iostream>
+#include "napiconfig.h"
 #include "NeXusFile.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World" << std::endl;
-    std::cout << "And Again" << std::endl;
-    return 0;
+  NeXus::File file("napi_cpp.nxs", NXACC_CREATE);
+  std::cout << "Hello World" << std::endl;
+  std::cout << "And Again" << std::endl;
+  return 0;
 }
