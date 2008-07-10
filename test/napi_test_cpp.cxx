27127c6a2fc911b84d700ee2a56eff58cb620abd
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2008-07-10 08:48:26 +0000

    Add in test program for CPP interface. Refs #116.
    git-svn-id: https://svn.nexusformat.org/code/trunk@1015 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

---
+++ test/napi_test_cpp.cxx
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "NeXusFile.hpp"
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello World" << std::endl;
+    std::cout << "And Again" << std::endl;
+    return 0;
+}
