d9245670251886c4cf6f166a8811436555ac9e33
Peter Peterson <petersonpf@ornl.gov>
2008-07-21 20:59:34 +0000

    Added documentation to some of the functions. Refs #116.
    git-svn-id: https://svn.nexusformat.org/code/trunk@1056 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- bindings/cpp/NeXusFile.hpp
+++ bindings/cpp/NeXusFile.hpp
@@ -7,7 +7,26 @@
 #include <vector>
 #include "napi.h"
 
+/**
+ * \file
+ * Documentation for the NeXus C++ API.
+ * 2000-2008 the NeXus group.
+ */
+
 namespace NeXus {
+  /**
+   * The primitive types published by this API.
+   * \li FLOAT32 float.
+   * \li FLOAT64 double
+   * \li INT8 int8_t
+   * \li UINT8 uint8_t
+   * \li INT16 int16_t
+   * \li UINT16 uint16_t
+   * \li INT32 int32_t
+   * \li UINT32 uint32_t
+   * \li INT64 int8_t if available on the machine
+   * \li UINT64 uint8_t if available on the machine
+   */
   enum NXnumtype {
     FLOAT32 = NX_FLOAT32,
     FLOAT64 = NX_FLOAT64,
@@ -24,6 +43,13 @@ namespace NeXus {
     BINARY = NX_BINARY
   };
 
+  /**
+   * The available compression types. These are all ignored in xml files.
+   * \li NONE no compression
+   * \li LZW Lossless Lempel Ziv Welch compression (recommended)
+   * \li RLE Run length encoding (only HDF-4)
+   * \li HUF Huffmann encoding (only HDF-4)
+   */
   enum NXcompression {
     NONE = NX_COMP_NONE,
     LZW = NX_COMP_LZW,
@@ -31,57 +57,128 @@ namespace NeXus {
     HUF = NX_COMP_HUF
   };
 
+  /**
+   * This structure holds the type and dimensions of a primative field/array.
+   */
   struct Info{
+    /** The primative type for the field. */
     NXnumtype type;
+    /** The dimensions of the file. */
     std::vector<int> dims;
   };
 
+  /** Information about an attribute. */
   struct AttrInfo{
+    /** The primative type for the attribute. */
     NXnumtype type;
+    /** The length of the attribute. */
     unsigned length;
+    /** The name of the attribute. */
     std::string name;
   };
 
+  /**
+   * The Object that allows access to the information in the file.
+   */
   class File
   {
   private:
+    /** The handle for the C-API. */
     NXhandle m_file_id;
+    /**
+     * \return A pair of the next entry available in a listing.
+     */
     std::pair<std::string, std::string> getNextEntry();
+    /**
+     * \return Information about the next attribute.
+     */
     AttrInfo getNextAttr();
+    /**
+     * This is a deprecated function.
+     * \param com The compression type.
+     */
     void compress(NXcompression comp);
-    void status2exception(int status);
 
   public:
+    /**
+     * Create a new File.
+     *
+     * \param filename The name of the file to open.
+     * \param access How to access the file.
+     */
     File(const std::string& filename, const NXaccess access = NXACC_READ);
-    
+
+    /** Destructor. This does close the file. */
     ~File();
 
+    /** Close the file before the constructor is called. */
     void close();
 
+    /** Flush the file. */
     void flush();
 
-    void makeGroup(const std::string& name, const std::string& class_name, bool open_group = false);
+    /**
+     * Create a new group.
+     *
+     * \param name The name of the group to create (i.e. "entry").
+     * \param class_name The type of group to create (i.e. "NXentry").
+     * \param open_group Whether or not to automatically open the group after 
+     * creating it.
+     */
+    void makeGroup(const std::string& name, const std::string& class_name,
+                   bool open_group = false);
 
+    /**
+     * Open an existing group.
+     *
+     * \param name The name of the group to create (i.e. "entry").
+     * \param class_name The type of group to create (i.e. "NXentry").
+     */
     void openGroup(const std::string& name, const std::string& class_name);
 
+    /**
+     * Open the NeXus object with the path specified.
+     *
+     * \param path A unix like path string to a group or field. The path 
+     * string is a list of group names and SDS names separated with a slash,
+     * '/' (i.e. "/entry/sample/name").
+     */
     void openPath(const std::string& path);
 
+    /**
+     * Open the group in which the NeXus object with the specified path exists.
+     *
+     * \param path A unix like path string to a group or field. The path 
+     * string is a list of group names and SDS names separated with a slash,
+     * '/' (i.e. "/entry/sample/name").
+     */
     void openGroupPath(const std::string& path);
 
+    /**
+     * Close the currently open group.
+     */
     void closeGroup();
 
+    /**
+     * Create a data field with the specified information.
+     *
+     * \param name The name of the field to create (i.e. "distance").
+     * \param type The primative type of the field (i.e. "NeXus::FLOAT32").
+     * \param dims The dimensions of the field.
+     * \param open_data Whether or not to open the data after creating it.
+     */
     void makeData(const std::string& name, NXnumtype type,
                   const std::vector<int>& dims, bool open_data = false);
 
+    /**
+     * Create a 1D data field with the specified information.
+     *
+     * \param name The name of the field to create (i.e. "distance").
+     * \param type The primative type of the field (i.e. "NeXus::FLOAT32").
+     * \param length The number of elements in the field.
+     * \param open_data Whether or not to open the data after creating it.
+     */
     void makeData(const std::string& name, const NXnumtype type,
-                  const int length, bool open_data = false);
-
-    template <typename NumT>
-    void makeData(const std::string& name,
-                  const std::vector<int>& dims, bool open_data = false);
-
-    template <typename NumT>
-    void makeData(const std::string& name, 
                   const int length, bool open_data = false);
 
     void writeData(const std::string& name, const std::string& value);
