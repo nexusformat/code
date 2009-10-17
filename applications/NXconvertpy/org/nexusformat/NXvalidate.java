db5774c2c65c92f6fd9886e96f33e869cd823e8a
Stuart Campbell <campbellsi@ornl.gov>
2009-10-17 23:32:44 +0000

    Dumb main method. refs #179
    git-svn-id: https://svn.nexusformat.org/code/trunk@1320 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

---
+++ applications/NXconvertpy/org/nexusformat/NXvalidate.java
@@ -0,0 +1,12 @@
+package org.nexusformat;
+
+public class NXvalidate {
+
+	static String VERSION = "0.1 alpha";
+
+	public static void main(String[] args) {
+	
+		System.out.println("Running NXvalidate (version:"+VERSION+")");
+	
+	}
+}
