9db0b6ba506a27275b1ec0f6c5bbbaa5005cebd8
Peter Peterson <petersonpf@ornl.gov>
2009-10-18 22:30:38 +0000

    Parse the information from the error report. Refs #179.
    git-svn-id: https://svn.nexusformat.org/code/trunk@1351 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- applications/NXconvertpy/org/nexusformat/SVRLitem.java
+++ applications/NXconvertpy/org/nexusformat/SVRLitem.java
@@ -4,6 +4,7 @@ import java.util.Vector;
 
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 public class SVRLitem {
 	private static String NS_START = "[namespace-uri";
@@ -11,9 +12,15 @@ public class SVRLitem {
 	
 	private String[] location;
 	private String xpath_str;
+	private String type;
+	private String test;
+	private String message;
 
 	public SVRLitem(final Node node) {
 		this.setLocation(node);
+		this.setType(node.getNodeName());
+		this.setAttrs(node.getAttributes());
+		this.setMessage(node);
 	}
 
 	Vector<String> getLocationArray() {
@@ -33,8 +40,55 @@ public class SVRLitem {
 		return buffer.toString();
 	}
 
+	public String getTest() {
+		return this.test;
+	}
+	
 	public String getMessage() {
-		return "Stuff is wrong";
+		return this.message;
+	}
+
+	private void setType(final String type) {
+		if (type.equals("svrl:failed-assert")) {
+			this.type = "failed assert";
+		} else {
+			throw new Error("Do not understand type " + type);
+		}
+	}
+
+	private void setAttrs(final NamedNodeMap attrs) {
+		Node attr;
+		// get the test name
+		attr = attrs.getNamedItem("test");
+		if (attr != null) {
+			this.test = attr.getNodeValue();
+		}
+	}
+	private void setMessage(final Node node) {
+		if (node.getNodeName().equals("svrl:text")) {
+			NodeList nodes = node.getChildNodes();
+			int size = nodes.getLength();
+			if (size == 0) {
+				this.message = "";
+			} else if(size == 1) {
+				this.message = nodes.item(0).getNodeValue();
+				this.message = this.message.trim();
+			} else {
+				throw new Error("Expected only 1 node, found "
+						+ nodes.getLength());
+			}
+		} else {
+			NodeList nodes = node.getChildNodes();
+			int size = nodes.getLength();
+			Node temp;
+			for (int i = 0; i < size; i++) {
+				temp = nodes.item(i);
+				if (temp.getNodeName().equals("svrl:text")) {
+					this.setMessage(temp);
+					return;
+				}
+			}
+		}
 	}
 
 	private void setLocation(final Node node) {
