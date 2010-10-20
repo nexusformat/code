a390a492b504d1ea4d104481a4a5c6895a751898
Pete Jemian <jemian@anl.gov>
2010-10-20 23:44:35 +0000

    refs #236, changed URL from old Argonne address to http://www.nexusformat.org, changed NXvalidata to NXvalidate in the help file
    git-svn-id: https://svn.nexusformat.org/code/trunk@1569 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- applications/NXvalidate/src/org/nexusformat/nxvalidate/NXvalidate.java
+++ applications/NXvalidate/src/org/nexusformat/nxvalidate/NXvalidate.java
@@ -18,7 +18,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * For further information, see <http://www.neutron.anl.gov/NeXus/>
+ * For further information, see <http://www.nexusformat.org/>
  *
  * NXvalidate.java
  *
--- applications/NXvalidate/src/org/nexusformat/nxvalidate/help/jhelpset.hs
+++ applications/NXvalidate/src/org/nexusformat/nxvalidate/help/jhelpset.hs
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="ISO-8859-1" standalone="no"?>
 <!DOCTYPE helpset PUBLIC "-//Sun Microsystems Inc.//DTD JavaHelp HelpSet Version 1.0//EN" "http://java.sun.com/products/javahelp/helpset_1_0.dtd">
 <helpset version="1.0">
-  <title>NXvalidata 1.0alpha - NeXus File Validator Tool.</title>
+  <title>NXvalidate 1.0alpha - NeXus File Validator Tool.</title>
   <maps>
     <homeID>top</homeID>
     <mapref location="jhelpmap.jhm"/>
