9c9ec8340618fa219df40ed08e2831fb106b8cf9
Stephen Rankin <s.e.rankin@rl.ac.uk>
2011-07-14 15:11:49 +0000

    Initial import of CMake Files ref#281.
    git-svn-id: https://svn.nexusformat.org/code/trunk@1607 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

---
+++ cmake_include/FindSystemConfig.cmake
@@ -0,0 +1,44 @@
+## Process this file with cmake
+#====================================================================
+#  NeXus - Neutron & X-ray Common Data Format
+#  
+#  CMakeLists for building the NeXus library and applications.
+#
+#  Copyright (C) 2010 Stephen Rankin
+#  
+#  This library is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU Lesser General Public
+#  License as published by the Free Software Foundation; either
+#  version 2 of the License, or (at your option) any later version.
+# 
+#  This library is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#  Lesser General Public License for more details.
+# 
+#  You should have received a copy of the GNU Lesser General Public
+#  License along with this library; if not, write to the Free 
+#  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, 
+#  MA  02111-1307  USA
+#             
+#  For further information, see <http://www.neutron.anl.gov/NeXus/>
+#
+#
+#====================================================================
+INCLUDE (CheckIncludeFile)
+
+CHECK_INCLUDE_FILE(stdint.h STDINT_INCLUDE)
+
+INCLUDE_DIRECTORIES(${STDINT_INCLUDE})
+
+ADD_DEFINITIONS(-DPRINTF_INT64="lld" -DPRINTF_UINT64="llu")
+
+IF(DEFINED STDINT_INCLUDE)
+    ADD_DEFINITIONS(-DHAVE_STDINT_H=1)
+ENDIF(DEFINED STDINT_INCLUDE)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SystemConfig DEFAULT_MSG STDINT_INCLUDE)
+
+MARK_AS_ADVANCED(STDINT_INCLUDE)
+
