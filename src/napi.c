82fad1f2471ef78273d2a789d459f57a704eb838
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2011-11-08 13:23:36 +0000

    Use PTHREAD_MUTEX_RECURSIVE_NP instead of PTHREAD_MUTEX_RECURSIVE (for RHEL5) Refs #300
    git-svn-id: https://svn.nexusformat.org/code/trunk@1745 ff5d1e40-2be0-497f-93bd-dc18237bd3c7

--- src/napi.c
+++ src/napi.c
@@ -99,7 +99,7 @@ static void nx_pthread_init()
 {
     pthread_mutexattr_t attr;
     pthread_mutexattr_init(&attr);
-    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE);
+    pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE_NP);
     pthread_mutex_init(&nx_mutex, &attr);
 }
 
