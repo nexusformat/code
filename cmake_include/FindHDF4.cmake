14a7502f08525e3a2031b87dbdb3e4b8acfce1f5
Pete Peterson <petersonpf@ornl.gov>
2015-08-11 16:52:47 -0400

    Cleaning up cmake logic.
    CMake already has some of the finders and logic.
    
    This really only works for the c-api so far.

--- cmake_include/FindHDF4.cmake
+++ cmake_include/FindHDF4.cmake
@@ -1,25 +1,25 @@
 ## Process this file with cmake
 #=============================================================================
 #  NeXus - Neutron & X-ray Common Data Format
-#  
+#
 #  CMakeLists for building the NeXus library and applications.
 #
 #  Copyright (C) 2011 Stephen Rankin
-#  
+#
 #  This library is free software; you can redistribute it and/or modify it
 #  under the terms of the GNU Lesser General Public License as published by the
 #  Free Software Foundation; either version 2 of the License, or (at your
 #  option) any later version.
-# 
+#
 #  This library is distributed in the hope that it will be useful, but WITHOUT
 #  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
 #  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
 #  for more details.
-# 
+#
 #  You should have received a copy of the GNU Lesser General Public License
 #  along with this library; if not, write to the Free Software Foundation,
 #  Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-#             
+#
 #  For further information, see <http://www.nexusformat.org>
 #
 #
@@ -32,25 +32,13 @@ find_module(HDF4
             HEADER_NAMES mfhdf.h
             )
 
-if(WITH_HDF4 AND NOT HAVE_HDF4)
+if(NOT HAVE_HDF4)
     #the user has explicitely requested HDF4 support but the required libraries
     #could not be found
     message(FATAL_ERROR "User requested HDF4 not found!")
-elseif(NOT WITH_HDF4 AND HAVE_HDF4 AND NOT WITHOUT_HDF4)  
-    #the user has not requested explicitely for HDF4 support - however, the 
-    #libraries have been found and we automatically switch on HDF4 support.
-    #Only used if HDF4 support has not been explicitely switched of by the user
-    set(WITH_HDF4 ON)
-    message(STATUS "Build with HDF4 support!")
-    message(STATUS "HDF4 header dir: ${HDF4_INCLUDE_DIRS}")
-    message(STATUS "HDF4 library dir: ${HDF4_LIBRARY_DIRS}")
-else()
-    message(STATUS "Build without HDF4 support!")
 endif()
 
 #------------------------------------------------------------------------------
 # search for additional packages required to link against HDF4
 #------------------------------------------------------------------------------
-if(WITH_HDF4)
-    find_package(JPEG REQUIRED)
-endif(WITH_HDF4)
+find_package(JPEG REQUIRED)
