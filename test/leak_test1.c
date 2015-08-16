67bc536c3ed93dae6bbc53ef1b21b211b34bd704
Eugen Wintersberger <eugen.wintersberger@gmail.com>
2015-08-16 13:04:08 +0200

    Added leak_test1.c
    Modifications are basically the same as for leak_test2.cxx and
    leak_test3.cxx.

--- test/leak_test1.c
+++ test/leak_test1.c
@@ -3,27 +3,40 @@
 #include <stdlib.h>
 #include <napi.h>
 
+#define ON_ERROR(msgstr)\
+{\
+    fprintf(stderr,msgstr);\
+    return 1;\
+}
+
 int main (int argc, char* argv[])
 {
 	NXaccess access_mode = NXACC_CREATE5;
-        const int nReOpen = 1000;
+    const int nReOpen = 1000;
 	printf("Running for %d iterations\n", nReOpen);
-        int iReOpen;
-        const char* szFile = "leak_test1.nxs";
+    int iReOpen;
+    const char* szFile = "leak_test1.nxs";
 
-        NXhandle fileid;
+    NXhandle fileid;
 	unlink(szFile);
-        if (NXopen(szFile, access_mode, &fileid) != NX_OK) return 1;
-        if( NXclose(&fileid) != NX_OK) return 1;
-        for( iReOpen = 0; iReOpen < nReOpen; iReOpen++ )
-        {
-                if( 0 == iReOpen % 100 )
-                        printf("loop count %d\n", iReOpen);
-                if( NXopen(szFile, NXACC_RDWR, &fileid ) != NX_OK) return 1;
-                if( NXclose(&fileid) != NX_OK ) return 1;
-        }      
+    if (NXopen(szFile, access_mode, &fileid) != NX_OK) 
+        ON_ERROR("NXopen failed!\n")
+        
+    if( NXclose(&fileid) != NX_OK)
+        ON_ERROR("NXclose failed!\n")
+
+    for( iReOpen = 0; iReOpen < nReOpen; iReOpen++ )
+    {
+        if( 0 == iReOpen % 100 ) printf("loop count %d\n", iReOpen);
+
+        if( NXopen(szFile, NXACC_RDWR, &fileid ) != NX_OK) 
+            ON_ERROR("NXopen failed!\n");
+
+        if( NXclose(&fileid) != NX_OK )
+            ON_ERROR("NXclose failed!\n");
+    }      
+    
 	unlink(szFile);
 	fileid = NULL;
-	_exit(EXIT_FAILURE);
-        return 0;
+    return 0;
 }
